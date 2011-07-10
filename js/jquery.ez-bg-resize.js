ab663b029db7e27722a5fba2be9448b784cb7f4b
Carlo kajyr <carlo.panzi@gmail.com>
2011-07-10 12:02:32 +0200

    Some optimization
    Moved the bind function inside the plugin.
    Saved the html elements to a local variable, to reduce (slow) DM access.

--- js/jquery.ez-bg-resize.js
+++ js/jquery.ez-bg-resize.js
@@ -8,6 +8,8 @@
 (function($) {
 	// Global Namespace
     var jqez = {};
+    var container;
+    var image;
 
     // Define the plugin
     $.fn.ezBgResize = function(options) {
@@ -28,91 +30,97 @@
 			
 			// Create a unique div container
 			$("body").append('<div id="jq_ez_bg"></div>');
-
+			
+			container = $("#jq_ez_bg");
+			
 			// Add the image to it.
-			$("#jq_ez_bg").html('<img src="' + jqez.img[0] + '" width="' + jqez.width + '" height="' + jqez.height + '" border="0">');
+			container.html('<img src="' + jqez.img[0] + '" width="' + jqez.width + '" height="' + jqez.height + '" border="0">');
 
 			// First position object
-	        $("#jq_ez_bg").css("visibility","hidden");
-
+			container.css({
+            	"position":"fixed",
+            	"top":"0px",
+  			    "left":"0px",
+       	    	"z-index":"-1",
+    	        "overflow":"hidden",
+				"opacity" : jqez.opacity,
+				"visibility": "hidden"
+  		    });
 			// Overflow set to hidden so scroll bars don't mess up image size.
 	        $("body").css({
 	            "overflow":"hidden"
 	        });
+	        
+	        image = container.children('img');
 
 			resizeImage();
+			$(window).bind("resize", function() {
+				resizeImage();
+			});
+			
 		});
     };
 
-	$(window).bind("resize", function() {
-		resizeImage();
-	});
 	
 	// Actual resize function
     function resizeImage() {
-	
-        $("#jq_ez_bg").css({
-            "position":"fixed",
-            "top":"0px",
-            "left":"0px",
-            "z-index":"-1",
-            "overflow":"hidden",
-            "width":$(window).width() + "px",
-            "height":$(window).height() + "px",
-			"opacity" : jqez.opacity
-        });
+		container.css({
+    	    "width":$(window).width() + "px",
+			"height":$(window).height() + "px"
+  		});
+        
 		
 		// Image relative to its container
-		$("#jq_ez_bg").children('img').css("position", "relative");
+		image.css("position", "relative");
 
         // Resize the img object to the proper ratio of the window.
-        var iw = $("#jq_ez_bg").children('img').width();
-        var ih = $("#jq_ez_bg").children('img').height();
+        var iw = image.width();
+        var ih = image.height();
         
         if ($(window).width() > $(window).height()) {
             //console.log(iw, ih);
             if (iw > ih) {
                 var fRatio = iw/ih;
-                $("#jq_ez_bg").children('img').css("width",$(window).width() + "px");
-                $("#jq_ez_bg").children('img').css("height",Math.round($(window).width() * (1/fRatio)));
+                image.css("width",$(window).width() + "px");
+                image.css("height",Math.round($(window).width() * (1/fRatio)));
 
                 var newIh = Math.round($(window).width() * (1/fRatio));
 
                 if(newIh < $(window).height()) {
                     var fRatio = ih/iw;
-                    $("#jq_ez_bg").children('img').css("height",$(window).height());
-                    $("#jq_ez_bg").children('img').css("width",Math.round($(window).height() * (1/fRatio)));
+                    image.css("height",$(window).height());
+                    image.css("width",Math.round($(window).height() * (1/fRatio)));
                 }
             } else {
                 var fRatio = ih/iw;
-                $("#jq_ez_bg").children('img').css("height",$(window).height());
-                $("#jq_ez_bg").children('img').css("width",Math.round($(window).height() * (1/fRatio)));
+                image.css("height",$(window).height());
+                image.css("width",Math.round($(window).height() * (1/fRatio)));
             }
         } else {
             var fRatio = ih/iw;
-            $("#jq_ez_bg").children('img').css("height",$(window).height());
-            $("#jq_ez_bg").children('img').css("width",Math.round($(window).height() * (1/fRatio)));
+            image.css("height",$(window).height());
+            image.css("width",Math.round($(window).height() * (1/fRatio)));
         }
 		
 		// Center the image
 		if (typeof(jqez.center) == 'undefined' || jqez.center) {
-			if ($("#jq_ez_bg").children('img').width() > $(window).width()) {
-				var this_left = ($("#jq_ez_bg").children('img').width() - $(window).width()) / 2;
-				$("#jq_ez_bg").children('img').css({
+			if (image.width() > $(window).width()) {
+				var this_left = (image.width() - $(window).width()) / 2;
+				image.css({
 					"top"  : 0,
 					"left" : -this_left
 				});
 			}
-			if ($("#jq_ez_bg").children('img').height() > $(window).height()) {
-				var this_height = ($("#jq_ez_bg").children('img').height() - $(window).height()) / 2;
-				$("#jq_ez_bg").children('img').css({
+			if (image.height() > $(window).height()) {
+				var this_height = (image.height() - $(window).height()) / 2;
+				image.css({
 					"left" : 0,
 					"top" : -this_height
 				});
 			}
 		}
 
-        $("#jq_ez_bg").css({
+        container.css({
 			"visibility" : "visible"
 		});
 
